08b9db798b565159de0745a7e32ff30b92054012
danielcossins11 <daniel.cossins@yahoo.com>
2015-08-18 14:28:54 -0500

    displaying family members

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,22 +2,29 @@ requirejs.config({
   baseUrl: './javascripts',
   paths: {
     'jquery': '../bower_components/jquery/dist/jquery.min',
-    'lodash': '../bower_components/lodash/lodash.min',
     'firebase': '../bower_components/firebase/firebase',
-    'hbs': '../bower_components/require-handlebars-plugin/hbs',
-    'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
-    'q': '../bower_components/q/q',
-    'matchHeight': '../bower_components/matchHeight/jquery.matchHeight-min'
+    'hbs': '../bower_components/require-handlebars-plugin/hbs'
   },
   shim: {
-    'bootstrap': ['jquery'],
-    'matchHeight': ['jquery'],
     'firebase': {
       exports: 'Firebase'
     }
   }
 });
 
-require(["delete", "add"], function(remove, add){
-  
+require(["jquery", "firebase", "hbs", "delete", "add"],
+  function($, _firebase, Handlebars, remove, add){
+  var firebaseRef = new Firebase("https://daniel-family.firebaseio.com/");
+  firebaseRef.child("family").on("value", function(family){
+    var members = family.val();
+    console.log(members);
+
+
+
+    require(["hbs!../templates/populate"], function(temp){
+      $('#family').html(temp(members));
+    });
+
+    
+  });
 });
